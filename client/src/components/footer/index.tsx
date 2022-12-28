4cca3bf63a841a1d2927fb9d8a388cf496b3cfcc
newways-dev <reactwebdew@gmail.com>
2022-12-28 19:40:09 +0300

    footer

--- client/src/components/footer/index.tsx
+++ client/src/components/footer/index.tsx
@@ -1,3 +1,57 @@
+import styles from './footer.module.scss'
+
+import payment from '../../assets/images/payment.png'
+
 export const Footer = () => {
-  return <div>footer</div>
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.links}>
+        <div className={styles.column}>
+          <h1>Categories</h1>
+          <span>Women</span>
+          <span>Men</span>
+          <span>Shoes</span>
+          <span>Accessories</span>
+          <span>New Arrivals</span>
+        </div>
+        <div className={styles.column}>
+          <h1>Links</h1>
+          <span>FAQ</span>
+          <span>Pages</span>
+          <span>Stores</span>
+          <span>Compare</span>
+          <span>Cookies</span>
+        </div>
+        <div className={styles.column}>
+          <h1>About</h1>
+          <span>
+            Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore. Lorem ipsum dolor sit
+            amet conse ctetur adipisicing elit, seddo eiusmod tempor incididunt
+            ut labore etdolore.
+          </span>
+        </div>
+        <div className={styles.column}>
+          <h1>Contact</h1>
+          <span>
+            Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore. Lorem ipsum dolor sit
+            amet conse ctetur adipisicing elit, seddo eiusmod tempor incididunt
+            ut labore etdolore.
+          </span>
+        </div>
+      </div>
+      <div className={styles.copyright}>
+        <div className={styles.logo}>
+          <span className={styles.logoText}>Lamastore</span>
+          <span className={styles.copyrightText}>
+            © Copyright 2023. All Rights Reserved
+          </span>
+        </div>
+        <div className={styles.payment}>
+          <img src={payment} alt='' />
+        </div>
+      </div>
+    </footer>
+  )
 }
